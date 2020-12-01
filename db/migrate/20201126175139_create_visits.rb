e5977fab6ef2e866a84d44b29121dc73cb15c920
github username <charliepbarth@gmail.com>
2020-12-01 18:14:43 -0500

    Created migrations

--- db/migrate/20201126175139_create_visits.rb
+++ db/migrate/20201126175139_create_visits.rb
@@ -1,7 +1,9 @@
 class CreateVisits < ActiveRecord::Migration[6.0]
   def change
     create_table :visits do |t|
-
+      t.integer :user_id
+      t.integer :planet_id
+      t.boolean :private, default: false
       t.timestamps
     end
   end
--- db/migrate/20201126175146_create_planets.rb
+++ db/migrate/20201126175146_create_planets.rb
@@ -1,7 +1,9 @@
 class CreatePlanets < ActiveRecord::Migration[6.0]
   def change
     create_table :planets do |t|
-
+      t.string :name
+      t.string :size
+      t.string :description
       t.timestamps
     end
   end
