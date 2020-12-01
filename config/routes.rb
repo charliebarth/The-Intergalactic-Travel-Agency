4239bbbe34a59da8fdc0191bfcbe8294456a906a
github username <charliepbarth@gmail.com>
2020-12-01 17:44:22 -0500

    Added model associations and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :planets
-  resources :visits
+  resources :planets only: [:index, :show]
+  resources :visits only: [:index, :show]
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :users, only: [:show] do
+    resources :visits
+  end
+ 
+  root 'users#create'
 end
