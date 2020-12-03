c167533838333a3241469d73204157ac5ea76477
github username <charliepbarth@gmail.com>
2020-12-03 16:55:28 -0500

    Setup Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+    before_action :find_user, only: [:show, :edit, :update, :destroy]
+    before_action :redirect_already_logged_in, only: [:new]
+    before_action :redirect_anon_users_to_home, only: [:show, :edit, :update, :destroy]
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      @user = User.new(user_params)
+  
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        render :new
+      end
+    end
+  
+    def edit
+      unless helpers.has_access
+        flash[:message] = "You do not have access to Edit this User"
+        render "partials/error"
+      end
+    end
+  
+    def update
+      if @user.update(user_params)
+        redirect_to user_path(@user)
+      else
+        render :edit
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name, :password, :bio)
+    end
+  
+    def find_user
+      @user = User.find_by_id(params[:id])
+      render_error(params[:id], User.name, user_index_path) unless @user
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+    def has_access
+        id = params[:user_id] || params[:id]
+        current_user == User.find_by_id(id)
+    end
 end
