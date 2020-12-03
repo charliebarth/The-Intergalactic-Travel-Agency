578efc3260cad806793f442433ca0556f6c5d7b1
github username <charliepbarth@gmail.com>
2020-12-02 22:14:06 -0500

    Added helper methods to App Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        @current_user ||= session[:current_user_id] &&
+        User.find_by(id: session[:current_user_id])
+    end
+
+    def redirect_to_signup_if_not_logged_in
+        redirect_to root_path unless session[:user_id]
+    end
 end
