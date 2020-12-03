6123cb1a85303c94b232d84f45c03a61d71c67ac
github username <charliepbarth@gmail.com>
2020-12-02 22:12:22 -0500

    Setup Planet Controller

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,2 +1,11 @@
 class PlanetsController < ApplicationController
+    before_action
+
+    def index
+        @planets = Planet.all
+    end
+
+    def show
+        @planet = Planet.find_by_id(params[:id])
+    end
 end
