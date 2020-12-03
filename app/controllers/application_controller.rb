10c5bbba491b67063249b53f3b8d57b3ba933680
github username <charliepbarth@gmail.com>
2020-12-03 16:56:36 -0500

    Added helper methods to App Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,29 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        @current_user ||= session[:current_user_id] &&
-        User.find_by(id: session[:current_user_id])
-    end
+    helper_method :current_user
+    helper_method :authenticated
+
+    private
 
     def redirect_to_signup_if_not_logged_in
         redirect_to root_path unless session[:user_id]
+    end
+
+    def redirect_already_logged_in
+        if authenticated
+          flash[:message] = "You are already logged in"
+          redirect_to user_path(current_user)
+        end
+    end
+
+    def redirect_anon_users_to_home
+        redirect_to root_path unless authenticated
+    end
+
+    def authenticated
+        !!current_user
+    end
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id])
     end
 end
