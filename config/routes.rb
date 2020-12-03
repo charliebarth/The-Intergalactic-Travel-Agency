f0309361c9716d4bbf0d5337e8a2f6e96c0cd629
github username <charliepbarth@gmail.com>
2020-12-03 16:56:04 -0500

    Removed User index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :planets, only: [:index, :show]
   resources :visits, only: [:index, :show]
   
