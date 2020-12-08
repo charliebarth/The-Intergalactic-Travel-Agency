93d7d267e885ab282e8a576ee4cd65ac056ccf08
github username <charliepbarth@gmail.com>
2020-12-07 20:35:12 -0500

    Added validations to User and Planet Models

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,4 +1,5 @@
 class Planet < ApplicationRecord
     has_many :visits
     has_many :users, through: :visits
+    validates_uniqueness_of :name
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :visits
     has_many :planets, through: :visits
+    validates_uniqueness_of :user_name
+    validates_uniqueness_of :password_digest
 end
