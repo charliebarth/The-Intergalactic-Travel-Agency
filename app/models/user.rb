9502ed85adbd33ecae0c11679aed9e2e94948665
github username <charliepbarth@gmail.com>
2020-12-07 23:49:22 -0500

    Removed validation of unique password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
     has_many :visits
     has_many :planets, through: :visits
     validates_uniqueness_of :user_name
-    validates_uniqueness_of :password_digest
+    
 end
