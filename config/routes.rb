37e9c005e01a7f86a5146ce72373d401e2e566fb
github username <charliepbarth@gmail.com>
2020-12-08 20:48:21 -0500

    Reduced number of visit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,13 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :planets, only: [:index, :show]
-  resources :visits, only: [:index, :show]
+  resources :visits, only: [:index, :new, :create]
   
   resources :users, only: [:show] do
-    resources :visits, only: [:new, :create, :index, :show, :edit, :update]
+    resources :visits, only: [:index, :new, :create]
   end
  
   root 'sessions#welcome'
-  
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy', as: 'logout'
